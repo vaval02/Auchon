27188d44f68e0b08d9388c029beaf8a52d05f938
vaval <vavalentine02@orange.fr>
2026-06-07 17:41:55 +0200

    Ajouter firebase-config.js pour Netlify

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Copy this file to firebase-config.js and fill with your Firebase Web app settings
+// You can get these from the Firebase console (Project settings)
+// IMPORTANT: use the Web app config, not the Android app config.
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyBLZTLLHUI1AG1821Q-gtLpppe4msIThYQ",
+  authDomain: "courses-b3e0e.firebaseapp.com",
+  projectId: "courses-b3e0e",
+  storageBucket: "courses-b3e0e.firebasestorage.app",
+  messagingSenderId: "947861714319",
+  appId: "1:947861714319:web:e6c3c6362c01d20da887fa",
+  measurementId: "G-96P1FG8TPP"
+};
+console.log('Firebase config loaded', window.firebaseConfig);
