a6704094a009fc40b1f3d343df854dcaf31cdb17
vaval <votre_email@gmail.com>
2026-06-07 16:40:49 +0200

    Init

---
+++ firebase-config.example.js
@@ -0,0 +1,11 @@
+// Copy this file to firebase-config.js and fill with your Firebase project settings
+// You can get these from the Firebase console (Project settings)
+
+window.firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
